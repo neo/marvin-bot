045e4883ca4fc263b1c7c8c1f242a652e8362240
Björn Andersson + Jason Milkins + Swayam Narain <pair+ba+jm+sn@pivotal.io>
2016-04-05 11:38:27 +0800

    Refactor the CommandValidatorTest
    [#116428411]

--- src/test/java/io/pivotal/singapore/models/CommandValidatorTest.java
+++ src/test/java/io/pivotal/singapore/models/CommandValidatorTest.java
@@ -4,91 +4,81 @@ import io.pivotal.singapore.marvin.commands.arguments.ArgumentFactory;
 import io.pivotal.singapore.marvin.commands.arguments.Arguments;
 import org.junit.Test;
 import org.springframework.validation.BeanPropertyBindingResult;
-import org.springframework.validation.DirectFieldBindingResult;
 import org.springframework.validation.Errors;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 public class CommandValidatorTest {
+    private static CommandValidator subject = new CommandValidator();
 
     @Test
     public void checkCommandName() {
-        Command command = new Command();
+        Command command = getInvalidCommand();
+        assertError(command, "name", "name.empty");
 
-        SubCommand subCommand = new SubCommand();
-        List<SubCommand> subCommands = Arrays.asList(subCommand);
-
-        command.setSubCommands(subCommands);
-
-        Errors errors = new BeanPropertyBindingResult(command, "command");
-        CommandValidator subject = new CommandValidator();
-
-        subject.validate(command, errors);
-
-        assertThat(errors.getFieldError("name").getCode(), is(equalTo("name.empty")));
-
-        errors = new BeanPropertyBindingResult(command, "command");
         command.setName("I pity the fool");
-        subject.validate(command, errors);
-
-        assertThat(errors.getFieldError("name").getCode(), is(equalTo("name.nospaces")));
+        assertError(command, "name", "name.nospaces");
     }
 
     @Test
     public void checkEndpoint() {
-        Command command = new Command();
-        command.setName("hello");
+        Command command = getValidCommand();
+        command.setEndpoint(null);
+        assertError(command, "endpoint", "endpoint.invalidUrl");
 
-        Errors errors = new BeanPropertyBindingResult(command, "command");
-        CommandValidator subject = new CommandValidator();
-
-        subject.validate(command, errors);
-        assertThat(errors.getFieldError("endpoint").getCode(), is(equalTo("endpoint.invalidUrl")));
-
-        errors = new BeanPropertyBindingResult(command, "command");
         command.setEndpoint("BLAAAARGH");
-
-        subject.validate(command, errors);
-        assertThat(errors.getFieldError("endpoint").getCode(), is(equalTo("endpoint.invalidUrl")));
+        assertError(command, "endpoint", "endpoint.invalidUrl");
 
         command.setEndpoint("https://hello.tld/1");
-        errors = new BeanPropertyBindingResult(command, "command");
-
-        subject.validate(command, errors);
-        assertThat(errors.getAllErrors(), empty());
-
-        command.setEndpoint("http://hello.tld/1");
-        errors = new BeanPropertyBindingResult(command, "command");
-
-        subject.validate(command, errors);
-        assertThat(errors.getAllErrors(), empty());
+        assertNoErrors(command);
     }
 
     @Test
     public void subCommandArguments() {
-        Command command = new Command();
-        command.setName("hello");
-        Arguments arguments = Arguments.of(Arrays.asList(
+        Command command = getValidCommand();
+        command.setSubCommands(getValidSubCommand());
+
+        assertError(command, "subCommands[0].arguments", "arguments.time.invalidUrl");
+    }
+
+    private Command getInvalidCommand() {
+        return new Command();
+    }
+
+    private Command getValidCommand() {
+        return new Command("time", "http://time.tld/");
+    }
+
+    private List<SubCommand> getValidSubCommand() {
+        Arguments arguments = Arguments.of(Collections.singletonList(
             ArgumentFactory.getWithEmptyArgument("time", "/hello")
         ));
 
         SubCommand subCommand = new SubCommand();
         subCommand.setArguments(arguments);
-        List<SubCommand> subCommands = Arrays.asList(subCommand);
-        command.setSubCommands(subCommands);
-
-        Errors errors = new DirectFieldBindingResult(command, "command");
-        CommandValidator subject = new CommandValidator();
-
-        subject.validate(command, errors);
-        assertThat(
-            errors.getFieldError("subCommands[0].arguments").getCode(),
-            is(equalTo("arguments.time.invalidUrl"))
-        );
+        return Collections.singletonList(subCommand);
     }
 
+    private Errors validate(Command command) {
+        Errors errors = new BeanPropertyBindingResult(command, "command");
+        subject.validate(command, errors);
+
+        return errors;
+    }
+
+    private void assertError(Command command, String fieldName, String errorCode) {
+        Errors errors = validate(command);
+
+        assertThat(errors.getFieldError(fieldName).getCode(), is(equalTo(errorCode)));
+    }
+
+    private void assertNoErrors(Command command) {
+        Errors errors = validate(command);
+
+        assertThat(errors.getAllErrors(), empty());
+    }
 }
