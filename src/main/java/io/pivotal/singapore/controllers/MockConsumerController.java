a2629dca01fd2ffdbc19574a3f914d2cb3165edd
Swayam Narain + Alan Yeo <pair+sn+ay@pivotal.io>
2016-04-05 14:23:19 +0800

    Add two additional endpoints to MockConsumerController for load testing
    Fix error in mocker endpoint to use the current API
    
    [#116851483]

--- src/main/java/io/pivotal/singapore/controllers/MockConsumerController.java
+++ src/main/java/io/pivotal/singapore/controllers/MockConsumerController.java
@@ -1,28 +1,38 @@
 package io.pivotal.singapore.controllers;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class MockConsumerController {
+    private static Map<String, Integer> counters = new HashMap<>();
+
+    @RequestMapping(value = "/counter", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> counter(@RequestParam String endpoint) {
+        HashMap<String, Object> response = new HashMap<>();
+        response.put("counter", counters.getOrDefault(endpoint, 0));
+        return response;
+    }
+
+    @RequestMapping(value = "/dummy", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String dummy() throws Exception {
+        incrementCounter("dummy");
+
+        throw new Exception("Dumb dumb!");
+    }
 
     @RequestMapping(value = "/mocker", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public Map<String, String> index(@RequestBody Map<String, Object> params) {
+        incrementCounter("mocker");
 
-        Map<String, String> arguments = (Map) params.get("arguments");
+        String location = (String) params.get("location");
         String response_message = "Its " + new Date();
 
-        if(arguments != null) {
-            String location = arguments.get("location");
-
+        if (location != null) {
             if(location.equals("England")) {
                 response_message = "It is Tea 'o' Clock";
             }
@@ -40,4 +50,11 @@ public class MockConsumerController {
 
         return response;
     }
+
+    private void incrementCounter(String endpoint) {
+        int counter = counters.getOrDefault(endpoint, 0);
+        counter++;
+        counters.put(endpoint, counter);
+    }
+
 }
