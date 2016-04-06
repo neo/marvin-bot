c2e893e540f36043912ec8261882f1e0a0388c42
Björn Andersson + Jason Milkins + Swayam Narain <pair+ba+jm+sn@pivotal.io>
2016-04-06 12:01:44 +0800

    Remove redundant import of Bool from apache xpath.

--- src/main/java/io/pivotal/singapore/models/ICommand.java
+++ src/main/java/io/pivotal/singapore/models/ICommand.java
@@ -1,6 +1,5 @@
 package io.pivotal.singapore.models;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import io.pivotal.singapore.marvin.commands.arguments.Arguments;
 import org.springframework.web.bind.annotation.RequestMethod;
 
