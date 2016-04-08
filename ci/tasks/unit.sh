5124db08dd6dfc24eb361092ae72f67a2aca2ee5
Björn Andersson <bandersson@pivotal.io>
2016-04-08 18:50:13 +0800

    Temp commit to display all env vars in task

--- ci/tasks/unit.sh
+++ ci/tasks/unit.sh
@@ -9,4 +9,5 @@ cd marvin-source
  ./gradlew test &&
     ./gradlew assemble &&
     cp -v build/libs/marvin-*.jar ../marvin-release/ &&
+    env &&
     echo ${BUILD_ID}_${BUILD_NAME}_${BUILD_JOB_NAME} > ../marvin-release/release-tag
