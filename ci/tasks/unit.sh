8c118015694fc1ab7ef09bff27c2838795820692
Swayam Narain + Alan Yeo <pair+sn+ay@pivotal.io>
2016-04-11 12:07:21 +0800

    include manifest file in marvin release

--- ci/tasks/unit.sh
+++ ci/tasks/unit.sh
@@ -9,4 +9,5 @@ cd marvin-source
  ./gradlew test &&
     ./gradlew assemble &&
     cp -v build/libs/marvin-*.jar ../marvin-release/ &&
+    cp -v manifest.yml ../marvin-release/ &&
     date "+%Y%m%d%H%M%S" > ../marvin-release/release-tag
