52277b0b0be2e32a3853e6b95fdb457e8a2ebad6
Alan Yeo <ayeo@pivotal.io>
2016-05-26 09:55:31 +0800

    Add error messages for validation

--- src/main/java/io/pivotal/singapore/marvin/slack/IncomingSlackRequest.java
+++ src/main/java/io/pivotal/singapore/marvin/slack/IncomingSlackRequest.java
@@ -7,10 +7,21 @@ import org.hibernate.validator.constraints.NotBlank;
 import java.util.Map;
 
 class IncomingSlackRequest extends ValidationObject<IncomingSlackRequest> {
-    @Getter @NotBlank final private String token;
-    @Getter @NotBlank final private String text;
-    @Getter @NotBlank final private String channelName;
-    @Getter @NotBlank final private String userName;
+    @Getter
+    @NotBlank(message = "Channel name cannot be empty")
+    final private String channelName;
+
+    @Getter
+    @NotBlank(message = "User name cannot be empty")
+    final private String userName;
+
+    @Getter
+    @NotBlank(message = "Token cannot be empty")
+    final private String token;
+
+    @Getter
+    @NotBlank(message = "Text cannot be empty")
+    final private String text;
 
     IncomingSlackRequest(Map<String, String> params) {
         this.channelName = params.getOrDefault("channel_name", null);
